7a3171b322c06522fcaeef2b7963b2647fd5c636
Muhammad Waleed Sajid <40654367+waleed-sajid@users.noreply.github.com>
2023-03-26 04:32:49 -0400

    WebSearchEngine

---
+++ src/WebSearchEngine/Cache.java
@@ -0,0 +1,96 @@
+package WebSearchEngine;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
+
+import org.apache.commons.io.FileUtils;
+
+public class Cache {
+
+	public static Boolean createCacheFile() {
+		if (!fileExists("cache.txt")) {
+			File fileStream = new File("cache.txt");
+			try {
+				if (fileStream.createNewFile()) {
+					// File created
+					// Utility.log("File created!");
+					return true;
+				}
+			} catch (Exception e) {
+				Utilities.log(e.getMessage());
+				return false;
+			}
+		} else {
+			// Utility.log("File already exists!");
+			return false;
+		}
+
+		return false;
+	}
+
+	public static Boolean fileExists(String path) {
+		File file = new File(path);
+		if (file.isFile()) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public static void addToCache(String filepath) {
+		File fileStream = new File("cache.txt");
+		// create new file if not existing
+		createCacheFile();
+		try {
+			// Append to file
+			BufferedWriter bufferWriter = new BufferedWriter(new FileWriter(fileStream, true));
+			bufferWriter.append(filepath);
+			bufferWriter.newLine();
+			bufferWriter.flush();
+			bufferWriter.close();
+		} catch (Exception e) {
+			Utilities.log(e.getMessage());
+		}
+
+	}
+
+	public static Boolean existsInCache(String path) {
+		BufferedReader reader;
+		try {
+			reader = new BufferedReader(new FileReader("cache.txt"));
+			String line = reader.readLine();
+			while (line != null) {
+				// Utility.log(line);
+
+				// Split the name from cache and take out the uri which will be first sequence
+				// before space as per our naming convention
+				String uri = line.split(" ")[0];
+				if (uri.equals(path)) {
+					// Utility.log(path+" exists");
+					reader.close();
+					return true;
+				}
+				line = reader.readLine();
+			}
+			reader.close();
+		} catch (Exception e) {
+			Utilities.log("Error Message: " + e.getMessage());
+		}
+		return false;
+	}
+
+	public static void deleteCache() {
+		try {
+			new FileOutputStream("cache.txt").close();
+			Utilities.log("Cache has been cleared");
+			FileUtils.cleanDirectory(new File("TextFiles"));
+		} catch (Exception e) {
+			Utilities.log(e.getMessage());
+		}
+	}
+
+}
---
+++ src/WebSearchEngine/Main.java
@@ -0,0 +1,173 @@
+package WebSearchEngine;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
+import Utilities.TST;
+
+import static WebSearchEngine.AutoCorrect.*;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		boolean state = true;
+		ArrayList<String> options = new ArrayList<String>() {
+			{
+				add("\n1. Search URL");
+				add("2. Remove Cache");
+				add("3. Search word");
+				add("4. Auto-correct word");
+				add("5. Auto-fill word");
+				add("6. Show history");
+				add("7. Delete history");
+				add("8. Exit");
+			}
+		};
+
+		do {
+			for (String option : options) {
+				Utilities.log(option);
+			}
+
+			Utilities.log("Please select one option:");
+			int option = input.nextInt();
+
+			switch (option) {
+
+				case 1: {
+					int depth = 1;
+					String uri = "";
+					Utilities.log("Selected option: " + options.get(option - 1));
+
+					input.nextLine();
+					// user input
+					Utilities.log("Enter Uri");
+					uri = input.nextLine();
+
+					// check if valid Uri
+					if (Pattern.matches(WebCrawler.urlPattern, uri)) {
+						// check if present in cache
+						if (!Cache.existsInCache(uri)) {
+							// calling crawler
+							try {
+								WebCrawler.checkCrawl(1, uri, new ArrayList<String>(), depth);
+							} catch (IOException e) {
+								Utilities.log(e.getMessage());
+							}
+						} else {
+							Utilities.log("This URL has already been crawled.");
+						}
+					} else {
+						Utilities.log("Invalid uri");
+					}
+					break;
+				}
+
+				case 2: {
+					Utilities.log("Selected option: " + options.get(option - 1));
+					Cache.deleteCache();
+					break;
+				}
+
+				case 3: {
+					Utilities.log("Selected option: " + options.get(option - 1));
+					try {
+						KeywordSearch.read_files();
+					} catch (Exception e) {
+						Utilities.log(e.getMessage());
+					}
+					break;
+				}
+
+				case 4: {
+					Utilities.log("Selected option: " + options.get(option - 1));
+					System.out.println("\nAUTOCORRECT SIMULATION\n");
+					loadWords();
+					sort(words);
+					startSimulation();
+					break;
+				}
+
+				case 5: {
+					Utilities.log("Selected option: " + options.get(option - 1));
+
+					input.nextLine();
+					Utilities.log("Enter word for suggestions (We use history.txt for this feature)");
+					String word = input.nextLine();
+					int top = 0;
+
+					ArrayList<String> keys = new ArrayList<String>();
+
+					// contains the history of searches by the user (for suggestions)
+					TST<Integer> history = new TST<Integer>();
+					try {
+						File myObj = new File("history.txt");
+						Scanner myReader = new Scanner(myObj);
+						// int i=0;
+						while (myReader.hasNextLine()) {
+							String data = myReader.nextLine();
+							keys.add(data.toLowerCase());
+							history.put(data.toLowerCase(), top);
+							top++;
+						}
+						myReader.close();
+					} catch (Exception e) {
+						Utilities.log("Error: " + e.getMessage());
+					}
+					history.printSimilarWords(word.toLowerCase());
+					break;
+				}
+
+				case 6: {
+					Utilities.log("Selected option: " + options.get(option - 1));
+					Utilities.log("\nHistory of Search:\n");
+					try {
+						File myObject = new File("history.txt");
+						Scanner FileReader = new Scanner(myObject);
+						int count = 1;
+						while (FileReader.hasNextLine()) {
+							String datahistory = FileReader.nextLine();
+							Utilities.log(count + ". " + datahistory);
+							count++;
+						}
+						Utilities.log("\nSelect One Option\n");
+						FileReader.close();
+					} catch (Exception c) {
+						Utilities.log("Error: " + c.getMessage());
+					}
+					break;
+				}
+
+				case 7: {
+					Utilities.log("Selected option:\n " + options.get(option - 1));
+					try {
+						new FileOutputStream("history.txt").close();
+						Utilities.log("Search history has been cleared!\n");
+						Utilities.log("\nSelect One Option\n");
+					} catch (Exception c) {
+						Utilities.log("Error:" + c.getMessage());
+					}
+					break;
+				}
+
+				case 8: {
+					Utilities.log("Selected option: " + options.get(option - 1));
+					state = false;
+					break;
+				}
+				default: {
+					Utilities.log("Selected option: " + options.get(option - 1));
+					break;
+				}
+			}
+		} while (state);
+
+		input.close();
+	}
+
+}
